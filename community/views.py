ac924e2ac7a7458bd08d6ec0565b5ac82e20fea0
KIMTAEKWAN <twan4220@gmail.com>
2022-03-28 14:46:43 +0000

    test

--- community/views.py
+++ community/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def calculator(request):
+    # return HttpResponse('계산기 기능 구현 시작입니다.')
+    
+    # data
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+
+    operators = request.GET.get('operators')
+    
+    # calc
+    if operators == '+':
+        result = int(num1) + int(num2)
+    elif operators == '-':
+        result = int(num1) - int(num2)
+    elif operators == '*':
+        result = int(num1) * int(num2)
+    elif operators == '/':
+        result = int(num1) / int(num2)
+    else:
+        result = 0
+
+
+    # response
+
+    return render(request, 'calculator.html', {'result': result})
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from community.views import calculator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('calculator/', calculator)
 ]
